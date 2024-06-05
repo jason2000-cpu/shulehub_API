223d8b8dc8a55e4ef824edde8c846c67f2c51c00
jason2000-cpu <jasonmwangi2000@gmail.com>
2024-06-05 15:04:38 +0300

    reconfiguring cors

--- api/server.js
+++ api/server.js
@@ -19,12 +19,12 @@
 
 
 const jsonServer = require('json-server');
-const cors = require('cors');
+// const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
+// server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
